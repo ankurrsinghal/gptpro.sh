466b7ea94e93fa285c19c8d5eafe50323b3cbc74
Ankur Singhal <ankurrsinghal@gmail.com>
2023-03-19 15:08:34 +0530

    scaffolded the svelte app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		Created by <a href="https://twitter.com/ankurpsinghal">Ankur Singhal</a>
+	</footer>
+</div>
+
+<style>
+	
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>ChatGPT</title>
+	<meta name="description" content="A new interface for ChatGPT" />
+</svelte:head>
+
+<section>
+	ChatGPT
+</section>
+
+<style>
+	
+</style>
