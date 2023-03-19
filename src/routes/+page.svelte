8573e55c9e3b2b4679e2d6f1e5928dfa28622636
Ankur Singhal <ankurrsinghal@gmail.com>
2023-03-20 05:16:00 +0530

    added sidebar collapse

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { ChatCompletion } from '$lib/GPT';
 	import Loader from '$lib/Loader.svelte';
-	import { scrollToBottomAction, hotKeyAction, textareaAutosizeAction } from 'svelte-legos';
+	import {
+		scrollToBottomAction,
+		hotKeyAction,
+		textareaAutosizeAction,
+		alertAction
+	} from 'svelte-legos';
 	import type { Bot, ChatConversation, ChatMessage } from '$lib/types';
 	import { onMount } from 'svelte';
 	import { conversationsStore, localStorageMiddleware } from '$lib/conversationsStore';
@@ -19,6 +24,8 @@
 	import { filtersStore } from '$lib/filterStore';
 	import { BotsList } from '$lib/Bots';
 	import BackIcon from '$lib/icons/BackIcon.svelte';
+	import LeftIcon from '$lib/icons/LeftIcon.svelte';
+	import RightIcon from '$lib/icons/RightIcon.svelte';
 
 	const conversations = localStorageMiddleware(conversationsStore());
 	let currentSelectedConversationId: string | null = null;
@@ -174,114 +181,138 @@
 	$: filteredBotsList = botsList.filter(
 		(bot) => bot.name.toLowerCase().indexOf(botsListFilterText.toLowerCase()) !== -1
 	);
+
+	let isSidebarVisible = true;
 </script>
 
 <section
 	class="w-screen h-screen flex"
 	use:hotKeyAction={{ code: 'Escape', cb: () => (isSettingsOpen = false) }}
 >
-	<div class="h-full w-[300px] bg-gray-100 relative overflow-auto border-r border-black">
-		<!-- sidebar -->
-		<div class="bg-white border-b border-black">
-			<!-- sidebar header -->
-			<div class="p-2 flex">
-				<button
-					class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
-					on:click={() => (isSettingsOpen = true)}
-				>
-					<SettingsIcon />
-					<span class="ml-2">Settings</span>
-				</button>
-				<button
-					class="ml-auto flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
-					on:click={handleFilterClick}
-					bind:this={filterRef}
-				>
-					<FunnelIcon />
-					<span class="ml-2">Filters</span>
-				</button>
-			</div>
-			<!-- <div>
-				<input class="p-2 w-full" />
-			</div> -->
-		</div>
-		<div>
-			{#each filteredConversations as conversation}
-				<Conversation
-					{conversation}
-					{handleConversationClick}
-					isSelected={currentSelectedConversationId === conversation.id}
-				/>
-			{/each}
-		</div>
-		<button
-			on:click={() => (isBotsListVisible = true)}
-			class="w-10 h-10 rounded-full bg-black flex items-center justify-center absolute right-4 bottom-4 z-10 text-white"
-		>
-			<PlusIcon />
-		</button>
-
-		{#if isBotsListVisible}
-			<div class="absolute inset-0 z-30 bg-slate-100">
-				<div class="bg-white p-2 border-b border-black">
-					<!-- bots list header -->
+	{#if isSidebarVisible}
+		<div class="h-full w-[300px] bg-gray-100 relative overflow-auto border-r border-black">
+			<!-- sidebar -->
+			<div class="bg-white border-b border-black">
+				<!-- sidebar header -->
+				<div class="p-2 flex">
 					<button
 						class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
-						on:click={() => (isBotsListVisible = false)}
+						on:click={() => (isSettingsOpen = true)}
 					>
-						<BackIcon />
-						<span class="ml-2">Back</span>
+						<SettingsIcon />
+						<span class="ml-2">Settings</span>
+					</button>
+					<button
+						class="ml-auto flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
+						on:click={handleFilterClick}
+						bind:this={filterRef}
+					>
+						<FunnelIcon />
+						<span class="ml-2">Filters</span>
+					</button>
+					<button
+						class="ml-auto flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
+						on:click={() => (isSidebarVisible = false)}
+					>
+						<LeftIcon />
 					</button>
 				</div>
-				<div class="bg-slate-100 p-2 border-b border-black">
-					<input
-						placeholder="🔎 Filter Bots"
-						class="text-sm w-full bg-white rounded-md px-2 py-1 border border-black"
-						bind:value={botsListFilterText}
-					/>
-				</div>
-				<!-- bots list -->
-				<div>
-					{#each filteredBotsList as bot}
-						<button
-							on:click={() => handleBotClick(bot)}
-							class="bg-slate-200 flex w-full p-4 border-b text-left border-black hover:bg-slate-100 cursor-pointer transition-colors"
-						>
-							{bot.name}
-						</button>
-					{/each}
-				</div>
+				<!-- <div>
+					<input class="p-2 w-full" />
+				</div> -->
 			</div>
-		{/if}
-	</div>
+			<div>
+				{#each filteredConversations as conversation}
+					<Conversation
+						{conversation}
+						{handleConversationClick}
+						isSelected={currentSelectedConversationId === conversation.id}
+					/>
+				{/each}
+			</div>
+			<button
+				on:click={() => (isBotsListVisible = true)}
+				class="w-10 h-10 rounded-full bg-black flex items-center justify-center absolute right-4 bottom-4 z-10 text-white"
+			>
+				<PlusIcon />
+			</button>
+
+			{#if isBotsListVisible}
+				<div class="absolute inset-0 z-30 bg-slate-100 flex flex-col">
+					<div class="bg-white p-2 border-b border-black">
+						<!-- bots list header -->
+						<button
+							class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
+							on:click={() => (isBotsListVisible = false)}
+						>
+							<BackIcon />
+							<span class="ml-2">Back</span>
+						</button>
+					</div>
+					<div class="bg-slate-100 p-2 border-b border-black">
+						<input
+							placeholder="🔎 Filter Bots"
+							class="text-sm w-full bg-white rounded-md px-2 py-1 border border-black"
+							bind:value={botsListFilterText}
+						/>
+					</div>
+					<!-- bots list -->
+					<div class="flex-1 overflow-auto">
+						<div>
+							{#each filteredBotsList as bot}
+								<button
+									on:click={() => handleBotClick(bot)}
+									class="bg-slate-200 flex w-full p-4 border-b text-left border-black hover:bg-slate-100 cursor-pointer transition-colors"
+								>
+									{bot.name}
+								</button>
+							{/each}
+						</div>
+					</div>
+				</div>
+			{/if}
+		</div>
+	{/if}
 
 	<div class="h-full flex-1">
 		{#if currentSelectedConversation}
 			<div class="h-full relative flex flex-col">
 				<!-- chat container -->
+				<div class="bg-white border-b border-black">
+					<div class="p-2 flex space-x-3">
+						{#if !isSidebarVisible}
+							<button
+								class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
+								on:click={() => (isSidebarVisible = true)}
+							>
+								<RightIcon />
+							</button>
+						{/if}
+						<button
+							class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
+							use:alertAction={{
+								title: 'Are you sure?',
+								description: "You won't be able to recover this conversation!",
+								onOk: handleDeleteClick
+							}}
+						>
+							<DeleteIcon />
+							<span class="ml-2">Delete</span>
+						</button>
+						<button
+							class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
+							on:click={handleArchiveClick}
+						>
+							<ArchiveIcon />
+							<span class="ml-2"
+								>{currentSelectedConversation.isArchived ? 'UnArchive' : 'Archive'}</span
+							>
+						</button>
+					</div>
+				</div>
 
 				<div class="overflow-auto bg-gray-100 relative max-h-full flex-1" use:scrollToBottomAction>
-					<div class="bg-white border-b border-black">
-						<div class="p-2 flex space-x-3">
-							<button
-								class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
-								on:click={handleDeleteClick}
-							>
-								<DeleteIcon />
-								<span class="ml-2">Delete</span>
-							</button>
-							<button
-								class="flex border border-black items-center justify-center text-sm rounded-md px-2 py-1"
-								on:click={handleArchiveClick}
-							>
-								<ArchiveIcon />
-								<span class="ml-2"
-									>{currentSelectedConversation.isArchived ? 'UnArchive' : 'Archive'}</span
-								>
-							</button>
-						</div>
-					</div>
-					<div class="p-8 space-y-6 text-sm min-w-full">
+					<div class="p-8 space-y-6 text-md min-w-full">
 						{#each currentSelectedConversation.messages as message}
 							<div
 								class={[
@@ -292,9 +323,10 @@
 									'rounded-md',
 									'shadow-md',
 									'leading-relaxed',
+									'border border-gray-300',
 									message.from === 'user'
-										? 'bg-white text-black ml-auto'
-										: 'bg-blue-100 mr-auto text-black'
+										? 'bg-cyan-100 text-black ml-auto'
+										: 'bg-white mr-auto text-black'
 								].join(' ')}
 							>
 								{message.content}
@@ -309,7 +341,7 @@
 				>
 					<textarea
 						use:textareaAutosizeAction
-						use:hotKeyAction={{ code: "Enter", cb: () => handleSend() }}
+						use:hotKeyAction={{ code: 'Enter', cb: () => handleSend() }}
 						disabled={isLoading}
 						bind:this={inputRef}
 						bind:value={currentMessagePrompt}
